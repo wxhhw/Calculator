10973668a4ccaf89130e911ccbe34e2ffb612c71
429AU <1017439429@qq.com>
2020-11-22 11:07:19 +0800

    函数名变更

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main(){
     int a, b, c;
     cout << "请输入a,b,c\n";
     cin >> a >> b >> c;
-    cout << "abc的积为：" << func1(a, b, c);
+    cout << "abc的积为：" << mul(a, b, c);
 
     int d, e, f;
     cout << "请输入d,e,f\n";
     cin >> d >> e >> f;
-    cout << "def的和为：" << func2(a, b, c);
+    cout << "def的和为：" << add(a, b, c);
     return 0;
 }
