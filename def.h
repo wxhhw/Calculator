357b6afdd8c233cf12e47472c17b7a371bce0e7f
429AU <1017439429@qq.com>
2020-11-22 09:57:00 +0800

    first commit

---
+++ def.h
@@ -0,0 +1,3 @@
+void func(int a,int b,int c){
+    return a*b*c;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "def.h" 
+using namespace std;  
+
+int main(){
+int a,b,c;
+cout << "请输入a,b,c\n";
+cin >> a >> b >> c;
+cout << "abc的乘积为："<<func(a,b,c);
+
+return 0;
+}
