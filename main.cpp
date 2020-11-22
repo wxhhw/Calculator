e4f679c8bf6ddac522f7e6463ff6bba01c88f544
429AU <1017439429@qq.com>
2020-11-22 09:58:12 +0800

    更改缩进

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@
 using namespace std;  
 
 int main(){
-int a,b,c;
-cout << "请输入a,b,c\n";
-cin >> a >> b >> c;
-cout << "abc的乘积为："<<func(a,b,c);
-
-return 0;
+    int a,b,c;
+    cout << "请输入a,b,c\n";
+    cin >> a >> b >> c;
+    cout << "abc的乘积为："<<func(a,b,c);
+    return 0;
 }
